eb4af16d13cf5cd8a4ac5d1503d9391b1d6e8b88
AJannik <jannik.schmidberger@gmail.com>
2023-08-11 12:10:30 +0200

    refactoring

--- Assets/Scripts/MeshGeneration.cs
+++ Assets/Scripts/MeshGeneration.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class MeshGeneration : MonoBehaviour
 {
@@ -7,6 +6,8 @@ public class MeshGeneration : MonoBehaviour
     public MeshFilter meshFilter;
     public float heightMultiplier = 1f;
     public MeshRenderer meshRenderer;
+
+    private MeshData meshData;
     
     void Start()
     {
@@ -22,63 +23,14 @@ public class MeshGeneration : MonoBehaviour
                 values[x, y] = pixels[y * width + x].r;
             }
         }
-        
-        meshFilter.mesh = GenerateMesh(values);
+
+        meshData = new MeshData(values, heightMultiplier);
+        meshFilter.mesh = meshData.CreateMesh();
     }
 
     void Update()
     {
         
-    }
-
-    private Mesh GenerateMesh(float[,] heightValues)
-    {
-        int width = heightValues.GetLength(0);
-        int depth = heightValues.GetLength(1);
-
-        Vector3[] vertices = new Vector3[width * depth];
-        Vector2[] uvs = new Vector2[width * depth];
-        int[] triangles = new int[(width - 1) * (depth - 1) * 6];
-
-        int vertexIndex = 0;
-        int triangleIndex = 0;
-
-        for (int y = 0; y < depth; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                vertices[vertexIndex] = new Vector3(x, heightValues[x, y] * heightMultiplier, y);
-                uvs[vertexIndex] = new Vector2(1f - x / (float)width, 1f - y / (float)depth);
-                
-                if (x < width - 1 && y < depth - 1)
-                {
-                    triangles[triangleIndex] = vertexIndex;
-                    triangles[triangleIndex + 1] = vertexIndex + width;
-                    triangles[triangleIndex + 2] = vertexIndex + width + 1;
-
-                    triangleIndex += 3;
-                    
-                    triangles[triangleIndex] = vertexIndex;
-                    triangles[triangleIndex + 1] = vertexIndex + width + 1;
-                    triangles[triangleIndex + 2] = vertexIndex + 1;
-                    
-                    triangleIndex += 3;
-                }
-                
-                vertexIndex++;
-            }
-        }
-
-        Mesh mesh = new Mesh
-        {
-            indexFormat = IndexFormat.UInt32,
-            vertices = vertices,
-            triangles = triangles,
-            uv = uvs
-        };
-
-        mesh.RecalculateNormals();
-        return mesh;
     }
 
     private void UpdateHeights(float[,] heightValues)
@@ -90,13 +42,8 @@ public class MeshGeneration : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                
+
             }
         }
-    }
-
-    private void AddTriangle()
-    {
-        
     }
 }
